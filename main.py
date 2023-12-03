c9c5b15f67a8b1e94a516d7b98408b20046f388b
MKM12345 <120132998+MKM12345@users.noreply.github.com>
2023-12-03 12:51:33 +0000

    Adapt MageAI class

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2023 MKM12345
 # Thanks to luizmelo at itch.io for SpriteSheets! Really appreciate it!
 import pygame
+import random
 from pygame import mixer
-from fighter import Fighter
+from fighter import Fighter, MageAI
 
 mixer.init()
 pygame.init()
@@ -12,7 +13,7 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Brawler")
+pygame.display.set_caption("xSim Duo MW Fight")
 
 # Set framerate
 clock = pygame.time.Clock()
@@ -27,6 +28,138 @@ WHITE = (255, 255, 255)
 intro_count = 3
 last_count_update = pygame.time.get_ticks()
 score = [0, 0]  # player scores. [P1, P2]
+round_over = False
+ROUND_OVER_COOLDOWN = 2000
+
+# Define fighter variables
+WARRIOR_SIZE = 162
+WARRIOR_SCALE = 4
+WARRIOR_OFFSET = [72, 56]
+WARRIOR_DATA = [WARRIOR_SIZE, WARRIOR_SCALE, WARRIOR_OFFSET]
+MAGE_SIZE = 250
+MAGE_SCALE = 3
+MAGE_OFFSET = [112, 107]
+MAGE_DATA = [MAGE_SIZE, MAGE_SCALE, MAGE_OFFSET]
+
+# Load music and sounds
+pygame.mixer.music.load("assets/audio/music.mp3")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1, 0.0, 5000)
+sword_fx = pygame.mixer.Sound("assets/audio/sword.wav")
+sword_fx.set_volume(0.5)
+magic_fx = pygame.mixer.Sound("assets/audio/magic.wav")
+magic_fx.set_volume(0.75)
+
+# Load background image
+bg_image = pygame.image.load("assets/images/background/background.jpg").convert_alpha()
+
+# Load spritesheets
+warrior_sheet = pygame.image.load("assets/images/warrior/Sprites/warrior.png").convert_alpha()
+mage_sheet = pygame.image.load("assets/images/mage/Sprites/mage.png").convert_alpha()
+
+# Load victory image
+victory_img = pygame.image.load("assets/images/icons/victory.png").convert_alpha()
+
+# Define number of steps in each animation
+WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
+MAGE_ANIMATION_STEPS = [8, 8, 1, 8, 8, 3, 7]
+
+# Define font
+count_font = pygame.font.Font("assets/fonts/turok.ttf", 80)
+score_font = pygame.font.Font("assets/fonts/turok.ttf", 30)
+
+# Function for drawing text
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
+# Function for drawing background
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0, 0))
+
+# Function for drawing fighter health bars
+def draw_health_bar(health, x, y):
+    ratio = health / 100
+    pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
+    pygame.draw.rect(screen, RED, (x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
+
+# Create two instances of fighters
+fighter_1 = Fighter(1, 200, 310, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx)
+mage_ai = MageAI(2, 700, 310, MAGE_DATA, mage_sheet, MAGE_ANIMATION_STEPS, magic_fx)
+
+# Game loop
+run = True
+while run:
+    clock.tick(FPS)
+
+    # Draw background
+    draw_bg()
+
+    # Show player stats
+    draw_health_bar(fighter_1.health, 20, 20)
+    draw_health_bar(mage_ai.health, 580, 20)
+    draw_text("P1: " + str(score[0]), score_font, RED, 20, 60)
+    draw_text("P2: " + str(score[1]), score_font, RED, 580, 60)
+
+    # Update countdown
+    if intro_count <= 0:
+        # Move fighters
+        fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, mage_ai, round_over)
+        mage_ai.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_1, round_over)
+
+        # Check for player defeat
+        if not round_over:
+            if not fighter_1.alive:
+                draw_text("Defeat", count_font, RED, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
+                score[1] += 1
+                round_over = True
+                round_over_time = pygame.time.get_ticks()
+            elif not mage_ai.alive:
+                draw_text("Victory", count_font, RED, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
+                score[0] += 1
+                round_over = True
+                round_over_time = pygame.time.get_ticks()
+    else:
+        # Display count timer
+        draw_text(str(intro_count), count_font, RED, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
+        # Update count timer
+        if (pygame.time.get_ticks() - last_count_update) >= 1000:
+            intro_count -= 1
+            last_count_update = pygame.time.get_ticks()
+
+    # Update fighters
+    fighter_1.update()
+    mage_ai.update()
+
+    # Draw fighters
+    fighter_1.draw(screen)
+    mage_ai.draw(screen)
+
+    # Display victory image
+    if round_over:
+        if not fighter_1.alive:
+            draw_text("Defeat", count_font, RED, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
+        elif not mage_ai.alive:
+            draw_text("Victory", count_font, RED, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)        
+        if pygame.time.get_ticks() - round_over_time > ROUND_OVER_COOLDOWN:
+            round_over = False
+            intro_count = 3
+            fighter_1 = Fighter(1, 200, 310, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx)
+            mage_ai = MageAI(2, 700, 310, MAGE_DATA, mage_sheet, MAGE_ANIMATION_STEPS, magic_fx)
+
+    # Event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    # Update display
+    pygame.display.update()
+
+# Exit pygame
+pygame.quit()
+
 round_over = False
 ROUND_OVER_COOLDOWN = 2000
 
